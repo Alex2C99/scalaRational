543c29423c3a58354fa1f3ea5df53ba3ad9c2468
Alexey Scripkin <a.scripkin@gmail.com>
2020-06-23 14:12:38 +0300

    inplisit test

--- src/Main.scala
+++ src/Main.scala
@@ -6,8 +6,14 @@ import Rational._
 
 object Main extends App {
 
-  println (Ratio(11,31).toChain)
-  println (fromChain(List(0, 2, 1, 4, 2)))
+    val r1 = Ratio(1,2)
+    val r2 = Ratio(1,3)
 
-  println (Ratio(49*49,49*11) pabs 7)
+    println( r1/r2 - r1*r2 )
+
+    val r3 = Ratio(17,37)
+    println (r3.toChain)
+    println (fromChain(List(0, 2, 5, 1, 2)))
+
+    println (Ratio(49,11) pabs 7)
 }
